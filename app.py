52782f52f9c364b6b06ded3ecde347b724e86c1c
Katie <klwalker2@sheffield.ac.uk>
2018-11-24 22:22:14 +0000

    Ready to deploy

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('./uploadToContacts', methods=['POST'])
+    def upload():
+        imagefile = flask.request.files.get('imagefile', '')
+        file.save(os.path.join(app.config['./known_people'], imagefile))
+
+@app.route('/uploadForCheck', methods=['POST'])
+    def check():
+    try:
+        imagefile = flask.request.files.get('imagefile', '')
+        file.save(os.path.join(app.config['./Unknown_pictures'], imagefile))
+        check_known()
+        os.remove(./Unknown_pictures/imagefile)
+    except Exception as err:
+        print("lol fail")
+
+def check_known()
+    return face_recognition --cpus 4 ./known_people/ ./Unknown_pictures/ cut -d ',' -f2
+    
+if __name__ == '__main__':
+    app.run(debug=True)
